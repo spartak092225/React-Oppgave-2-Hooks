8e81efad605acf6d85142c8c52eee3f909ba359b
Spartak092225 <spartak092225@gmail.com>
2025-10-20 14:06:41 +0200

    Refactor CookieCounter component for consistent import style and update image source path

--- src/components/CookieCounter.jsx
+++ src/components/CookieCounter.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import styles from './CookieCounter.module.css';
+import styles from "./CookieCounter.module.css";
 
 function CookieCounter() {
   const [count, setCount] = useState(0);
@@ -10,17 +10,10 @@ function CookieCounter() {
   }
 
   return (
-    <div className={styles['cookie-counter-container']}>
-      <p className={styles['cookie-number']}>{count}</p>
-      <button
-        onClick={oneMoreCookie}
-        className={styles['cookie-btn']}
-      >
-        <img
-          src="public\cookie-1.png"
-          alt="image of a cookie"
-          width="200px"
-        />
+    <div className={styles["cookie-counter-container"]}>
+      <p className={styles["cookie-number"]}>{count}</p>
+      <button onClick={oneMoreCookie} className={styles["cookie-btn"]}>
+        <img src="/public/cookie-1.png" alt="image of a cookie" width="200px" />
       </button>
     </div>
   );
